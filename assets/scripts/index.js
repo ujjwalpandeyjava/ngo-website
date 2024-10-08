3a35b433b8a1196187557b407af79628181e395b
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2024-10-08 23:41:32 +0530

    Added - On home page - Display image with css - Our work section with the count increasing feature and css.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,35 @@
 // Keep the code in the callback function, to work after dom load.
+let examplesTimer = {
+	1: null,
+	2: null,
+	3: null
+}
 $(document).ready(function () {
-	$(".home").css("background-color", "orange");
+	$(window).on('scroll load', function () {
+		let $element = $('.workExamples');
+
+		if ($element?.length > 0) {
+			let windowHeight = $(window).height();
+			let elementTop = $element.offset().top;
+			let elementBottom = elementTop + $element.outerHeight();
+			let scrollTop = $(window).scrollTop();
+			if (elementBottom > scrollTop && elementTop < scrollTop + windowHeight) {
+				let eachWorkExample = $(".eachWorkExample");
+				eachWorkExample.each((index, eachEg) => {
+					let countShowDiv = eachEg.querySelector(".count")
+					let totalCountToShow = countShowDiv.getAttribute("data-show_count")
+					let countSpan = countShowDiv.querySelector("span")
+					let count = Number(countSpan.textContent)
+					if (count < totalCountToShow) {
+						examplesTimer[index] = setInterval(() => {
+							if (Number(countSpan.textContent) >= totalCountToShow)
+								clearInterval(examplesTimer[index])
+							else
+								countSpan.innerText = Number(countSpan.textContent) + 10
+						}, 1.5);
+					}
+				});
+			}
+		}
+	});
 });
